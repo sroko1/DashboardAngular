37d2dc904e026045e33aa89533b490f4107338d9
sroko1 <70091227+sroko1@users.noreply.github.com>
2021-05-29 08:38:43 +0200

    somethings wrong with outlet-routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeAnim', [
+      transition('* => *', [
+        style({
+          background: 'blue'
+        }),
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-personal-dashboard';
+
+  prepareRoute(outlet: RouterOutlet) {
+    if (outlet.isActivated) {
+      
+    } return outlet.activatedRoute.snapshot.url
+  }
 }
+
+
